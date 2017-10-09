fb6b9e9b438eed6c56d8d94f39ff5e9ea14aaba2
bambangadrian <bambang.adrian@gmail.com>
2017-10-09 20:42:39 +0700

    EStore - Customer, shopping cart, and item on shopping cart.

--- oop/Tests/EStore.php
+++ oop/Tests/EStore.php
@@ -11,7 +11,23 @@
  * @link      -
  */
 require_once __DIR__ . '/../../vendor/autoload.php';
+echo '<pre>';
 $product = new \PhpTrain\Exercise\EStore\Product('ES001', 'TV LG 32 Inch', 15);
 $product->setPrice(1500000);
-echo '[' . $product->getId() . '] ' . $product->getName() . ' (Stock: ' . $product->getStock(
-    ) . ' - Price: ' . $product->getPrice() . ')';
+$cart = new \PhpTrain\Exercise\EStore\ShoppingCart();
+$cart->addItem($product, 5);
+$customer = new \PhpTrain\Exercise\EStore\Customer('CUST001', 'Bambang');
+//$cart->setOwner($customer);
+$customer->setShoppingCart($cart);
+$product2 = new \PhpTrain\Exercise\EStore\Product('SN001', 'Taro', 25);
+$product2->setPrice(5000);
+$customerCart = $customer->getShoppingCart();
+$customerCart->addItem($product2, 10);
+$customerCartContent = $customerCart->getCartContents();
+foreach ($customerCartContent as $item) {
+    $itemProduct = $item->getProductInstance();
+    echo $itemProduct->getId() . ' | ' . $itemProduct->getName() . ' | ' . $itemProduct->getPrice(
+        ) . ' | ' . $item->getItemQuantity();
+    echo '<br />';
+}
+$product2->setPrice(6000);
